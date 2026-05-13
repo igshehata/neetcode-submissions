6caf36ba90c3b92149df924623796e7922b86b45
Islam Shehata <38216118+igshehata@users.noreply.github.com>
2026-05-13 18:07:56 +0300

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums) {
+        let obj = {}
+        // let out = false
+        for (let i =0; i < nums.length; i++) {
+            if(obj[nums[i]]) {
+                return true;
+            } else {
+                obj[nums[i]] = true;
+            }
+        }
+        return false;
+    }
+}
